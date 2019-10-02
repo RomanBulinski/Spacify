959ec54b871b4df105255a77d1ec8644859464b9
roman <aboandrus@gmail.com>
2019-10-02 20:36:00 +0200

    Implemented code

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Start {
 
     public static String spacify(String str){
-        return str; //show me the code!
+        return Arrays.asList(str.split("")).stream().map(letter -> letter+" ").collect( Collectors.joining( "" ) ).trim();
+    }
+
 
 }
